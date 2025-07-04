5dba9808583df64e5d99f07afc3365cdf686d332
Zeni <izzyzennah@outlook.com>
2025-07-04 19:31:35 +0200

    Refactor FavsHomepage to dynamically render SimpleFoodCard components from crops data; add crops.json for crop details

--- src/comps/mini/FavsHomepage.jsx
+++ src/comps/mini/FavsHomepage.jsx
@@ -1,6 +1,6 @@
 import SimpleFoodCard from "./SimpleFoodCard";
-import Turnip from "../../assets/imgs/Turnip.png";
 import { ChevronRight } from "lucide-react";
+import crops from "../../assets/json/crops.json";
 
 const FavsHomepage = () => {
   return (
@@ -11,38 +11,18 @@ const FavsHomepage = () => {
       </div>
 
       <div className="grid grid-cols-2 sm:grid-cols-3 gap-4">
-        <SimpleFoodCard
-          name="Turnip"
-          icon={Turnip}
-          season="spring"
-          seedPrice={25}
-          sellPrice={40}
-          isFavorited={false}
-        />
-        <SimpleFoodCard
-          name="Chili Pepper"
-          icon="https://fieldsofmistria.wiki.gg/images/6/60/Chili_pepper.png?ebfc25"
-          season="summer"
-          seedPrice={40}
-          sellPrice={75}
-          isFavorited={false}
-        />
-        <SimpleFoodCard
-          name="Turnip and Potato Gratin"
-          icon="https://fieldsofmistria.wiki.gg/images/3/3f/Turnip_and_potato_gratin.png?bcf8c9&format=original"
-          season=""
-          seedPrice={25}
-          sellPrice={40}
-          isFavorited={false}
-        />
-        <SimpleFoodCard
-          name="Apple"
-          icon="https://fieldsofmistria.wiki.gg/images/7/7d/Apple.png?bfa8b3"
-          season="autumn"
-          seedPrice={400}
-          sellPrice={45}
-          isFavorited={false}
-        />
+        {crops.map(({ id, name, image, season, seedPrice, sellPrice }) => (
+          <SimpleFoodCard
+            key={id}
+            id={id}
+            name={name}
+            icon={image}
+            season={season}
+            seedPrice={seedPrice}
+            sellPrice={sellPrice}
+            isFavorited={false}
+          />
+        ))}
       </div>
     </>
   );
--- src/comps/mini/SimpleFoodCard.jsx
+++ src/comps/mini/SimpleFoodCard.jsx
@@ -1,4 +1,12 @@
-import { Flower, Sun, Leaf, Snowflake, Heart, BadgeCent } from "lucide-react";
+import {
+  Flower,
+  Sun,
+  Leaf,
+  Snowflake,
+  Heart,
+  BadgeCent,
+  TreePine,
+} from "lucide-react";
 
 const seasonMap = {
   spring: { icon: Flower, color: "bg-green-100 text-green-700" },
@@ -17,10 +25,10 @@ export default function SimpleFoodCard({
 }) {
   const lowerSeason = season?.toLowerCase();
   const seasonInfo = seasonMap[lowerSeason];
+  const isForage = seedPrice === 0;
 
   return (
-    <div className="relative w-full h-56 bg-white rounded-2xl shadow-sm border border-slate-200 p-4 py-5 flex flex-col justify-between overflow-hidden">
-      {/* Season Icon Top-Right (conditionally rendered) */}
+    <div className="relative w-full h-56 bg-white rounded-2xl shadow-sm border border-slate-200 p-4 py-5 flex flex-col justify-between overflow-hidden hover:bg-slate-100 hover:shadow-md">
       {seasonInfo && (
         <div
           className={`absolute top-0 right-0 p-2 rounded-bl-xl ${seasonInfo.color}`}
@@ -29,12 +37,10 @@ export default function SimpleFoodCard({
         </div>
       )}
 
-      {/* Icon */}
       <div className="flex justify-center mt-2">
         <img src={icon} alt={name} className="w-22 h-22" />
       </div>
 
-      {/* Name */}
       <h3
         className={`text-center font-medium mt-2 w-full h-10 flex justify-center items-center ${
           name.length > 14 ? "text-base" : "text-xl"
@@ -43,17 +49,21 @@ export default function SimpleFoodCard({
         {name}
       </h3>
 
-      {/* Prices */}
       <div className="text-center text-lg mt-1 flex items-center gap-2 justify-center">
-        <p className="font-semibold text-gray-800 flex items-center gap-1">
-          <BadgeCent className="fill-yellow-100 text-yellow-500 w-5" />
-          {seedPrice}t
-        </p>
+        {isForage ? (
+          <p className="font-semibold text-green-200 flex justify-center items-center gap-1 w-6 h-6 bg-green-700 rounded-full">
+            <TreePine className="w-4 h-4 " />
+          </p>
+        ) : (
+          <p className="font-semibold text-gray-800 flex items-center gap-1">
+            <BadgeCent className="fill-yellow-100 text-yellow-500 w-5" />
+            {seedPrice}t
+          </p>
+        )}
         <p>·</p>
         <p className="text-gray-500">{sellPrice}t</p>
       </div>
 
-      {/* Heart Top-Left */}
       <div className="absolute top-0 left-0 p-2 rounded-br-xl text-gray-400 bg-red-100">
         <Heart
           className={`w-5 h-5 ${
