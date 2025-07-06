942257ffbcec3fc8d8fe4d623211224bc32eb24c
Zeni <izzyzennah@outlook.com>
2025-07-06 22:39:44 +0200

    Refactor Fish and Recipes components to improve layout and integrate SearchBar for enhanced user interaction

--- src/pages/Fish.jsx
+++ src/pages/Fish.jsx
@@ -1,7 +1,26 @@
+import { ChevronLeft, ListFilter } from "lucide-react";
+import SearchBar from "../comps/mini/SearchBar";
+import { Link } from "react-router-dom";
+
 const Fish = () => {
   return (
-    <div>Fish</div>
-  )
-}
+    <div className="p-[1rem]">
+      <div className="w-full flex items-center justify-between text-gray-600 pt-2 h-[64px] pb-4">
+        <Link to="/">
+          <ChevronLeft size="28" />
+        </Link>
+        <h1 className="text-2xl font-semibold">All Fish</h1>
+        <ListFilter size="28" />
+      </div>
 
-export default Fish
+      <SearchBar
+        placeholder="Search fish..."
+        // onChange={setQuery}
+        // onEnter={(q) => setSearchParams({ query: q })}
+        // initialValue={initialQuery}
+      />
+    </div>
+  );
+};
+
+export default Fish;
--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -1,7 +1,26 @@
+import { ChevronLeft, ListFilter } from "lucide-react";
+import SearchBar from "../comps/mini/SearchBar";
+import { Link } from "react-router-dom";
+
 const Recipes = () => {
   return (
-    <div>Recipes</div>
-  )
-}
+      <div className="p-[1rem]">
+        <div className="w-full flex items-center justify-between text-gray-600 pt-2 h-[64px] pb-4">
+          <Link to="/">
+            <ChevronLeft size="28" />
+          </Link>
+          <h1 className="text-2xl font-semibold">All Recipes</h1>
+          <ListFilter size="28" />
+        </div>
+  
+        <SearchBar
+          placeholder="Search recipes..."
+          // onChange={setQuery}
+          // onEnter={(q) => setSearchParams({ query: q })}
+          // initialValue={initialQuery}
+        />
+      </div>
+    );
+};
 
-export default Recipes
+export default Recipes;
