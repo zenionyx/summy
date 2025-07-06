08f74ec2e6984287d5a63d10d9bb43bc786a1ce7
Zeni <izzyzennah@outlook.com>
2025-07-06 15:28:56 +0200

    Add dynamic routing to Search page; redirect to CropDetail if an ID is present

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,9 +2,12 @@ import { useState } from "react";
 import crops from "../assets/json/crops.json";
 import SimpleFoodCard from "../comps/mini/SimpleFoodCard";
 import SearchNav from "../comps/mini/SearchNav";
+import { useParams, Link } from "react-router-dom";
 
 export default function Search() {
   const [query, setQuery] = useState("");
+  const { id } = useParams();
+  if (id) return <CropDetail />;
 
   // Filter crops based on `startsWith`, case-insensitive
   const filteredCrops = crops.filter((crop) =>
@@ -18,15 +21,17 @@ export default function Search() {
 
       <div className="grid grid-cols-2 sm:grid-cols-3 gap-4">
         {(query ? filteredCrops : crops).map((crop) => (
-          <SimpleFoodCard
-            key={crop.id}
-            name={crop.name}
-            icon={crop.image}
-            season={crop.season}
-            seedPrice={crop.seedPrice}
-            sellPrice={crop.sellPrice}
-            isFavorited={false}
-          />
+          <Link key={crop.id} to={`/crops/${crop.id}`}>
+            <SimpleFoodCard
+              key={crop.id}
+              name={crop.name}
+              icon={crop.image}
+              season={crop.season}
+              seedPrice={crop.seedPrice}
+              sellPrice={crop.sellPrice}
+              isFavorited={false}
+            />
+          </Link>
         ))}
       </div>
     </div>
